117f90af0f36d6de8a13757c320ebb73cb862373
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2024-03-16 21:26:13 +0800

    map notes as individual note components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      {notes.map((note) => {
+        return (
+          <Note key={note.key} title={note.title} content={note.content} />
+        );
+      })}
       <Footer></Footer>
-      <Note></Note>
     </div>
   );
 }
